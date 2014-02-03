53fac9e4b8f296cae77928a8c07561f49d7796ea
TakT <kirill.bereza@yandex.ru>
2014-02-03 16:58:54 +0600

    test remove

--- plugins/kmdiscount/coupons/install.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `#__ksenmart_discount_coupons` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `discount_id` int(11) NOT NULL,
-  `code` varchar(255) NOT NULL DEFAULT '',
-  `published` int(2) NOT NULL DEFAULT '1',
-  `used` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
--- plugins/kmdiscount/coupons/uninstall.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE
-`#__ksenmart_discount_coupons`;
