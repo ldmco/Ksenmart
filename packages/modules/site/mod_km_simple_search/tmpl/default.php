f8665b03977393fa519796e8205f6d41bb67c9b6
TakT <kirill.bereza@yandex.ru>
2014-08-02 19:22:59 +0600

    KSM[FIX]: Working ksenmart search form from enable routing

--- packages/modules/site/mod_km_simple_search/tmpl/default.php
+++ packages/modules/site/mod_km_simple_search/tmpl/default.php
@@ -3,12 +3,12 @@
     $value  = JRequest::getVar('value','');
 ?>
 <div class="search">
-    <form action="<?php echo JRoute::_('index.php?option=com_ksenmart&view=search&Itemid='.$Itemid); ?>" method="get" id="simple-search-form">
+    <form action="<?php echo JRoute::_('index.php?option=com_ksenmart&view=search&Itemid=' . $Itemid); ?>" method="POST" id="simple-search-form">
         <div class="input-append row-fluid">
             <input type="search" class="inputbox span11" name="value" placeholder="Начните вводить название товара" value="<?php echo $value; ?>" autocomplete="off" />
             <button type="submit" class="button btn">Поиск</button>
         </div>
-        
+
         <input type="hidden" name="option" value="com_ksenmart" />
         <input type="hidden" name="view" value="search" />
         <input type="hidden" name="Itemid" value="<?php echo $Itemid; ?>" />
@@ -18,7 +18,7 @@
             <h4 class="empty_result clearfix">Нет соответсвующих товаров</h4>
             <div class="items"></div>
             <div class="other_result">
-                <a href="<?php echo JRoute::_('index.php?option=com_ksenmart&view=search&Itemid='.$Itemid.'&value='); ?>" title="Остальные результаты поиска" class="button">Остальные результаты поиска</a>
+                <a href="<?php echo JRoute::_('index.php?option=com_ksenmart&view=search&Itemid=' . $Itemid.'&value='); ?>" title="Остальные результаты поиска" class="button">Остальные результаты поиска</a>
             </div>
         </div>
     </div>
