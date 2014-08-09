64e05a8c1503bcec7f38d97c52997d1372156331
TakT <kirill.bereza@yandex.ru>
2014-08-09 17:23:46 +0600

    KSM[FIX]: Added sql from to 3.1.2b up 3.1.4

--- packages/com_ksenmart/install.php
+++ packages/com_ksenmart/install.php
@@ -43,6 +43,30 @@ class com_ksenmartInstallerScript {
 		$table->load($extension->extension_id);
 		$table->params = $params;
 		$table->store();
+
+		$query = '
+			CREATE TABLE IF NOT EXISTS `#__ksenmart_files` (
+			  `id` int(10) NOT NULL AUTO_INCREMENT,
+			  `owner_id` int(10) NOT NULL,
+			  `media_type` varchar(15) NOT NULL,
+			  `owner_type` varchar(256) NOT NULL,
+			  `folder` varchar(32) NOT NULL,
+			  `filename` varchar(256) NOT NULL,
+			  `mime_type` varchar(32) NOT NULL,
+			  `title` varchar(256) NOT NULL,
+			  `ordering` int(10) NOT NULL,
+			  `params` text NOT NULL,
+			  PRIMARY KEY (`id`),
+			  KEY `owner_id` (`owner_id`),
+			  KEY `media_type` (`media_type`),
+			  KEY `owner_type` (`owner_type`),
+			  KEY `folder` (`folder`)
+			) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+			INSERT `#__ksenmart_files` SELECT * FROM `#__ksen_files` AS `ksf` WHERE `ksf`.`owner_type`!=\'user\';
+		';
+		$db->setQuery($query);
+		$db->execute();
 		
 		JFolder::delete($path);
 	}
