19431d125675909e7cca49661c03c236ecfe2292
Pasha <tominpasha@mail.ru>
2014-09-08 15:51:31 +0700

    KSM[fix]:fix order statuses module

--- packages/modules/admin/mod_km_order_statuses/js/orders.js
+++ packages/modules/admin/mod_km_order_statuses/js/orders.js
@@ -1,12 +1,13 @@
-var OrderStatusesModule=new KMListModule({
-	'module':'mod_km_order_statuses',
-	'view':'orders',
-	'table':'orderstatuses',
-	'sortable':false
-});
+var OrderStatusesModule = '';
 
 jQuery(document).ready(function(){
 
-	OrderStatusesModule.list=OrdersList;
+	OrderStatusesModule=new KMListModule({
+		'module':'mod_km_order_statuses',
+		'list':OrdersList,
+		'view':'orders',
+		'table':'orderstatuses',
+		'sortable':false
+	});
 
 });
