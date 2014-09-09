95d320f552a72a23947baab923cb2af2e0d3e0cb
Pasha <tominpasha@mail.ru>
2014-09-10 02:27:48 +0700

    KSM[fix]:fix renew userfields/usergroups modules

--- packages/modules/admin/mod_ks_userfields/js/users.js
+++ packages/modules/admin/mod_ks_userfields/js/users.js
@@ -3,7 +3,7 @@ var UserFieldsModule='';
 jQuery(document).ready(function(){
 
 	UserFieldsModule=new KMListModule({
-		'module':'mod_km_userfields',
+		'module':'mod_ks_userfields',
 		'view':'users',
 		'table':'userfields',
 		'sortable':true
--- packages/modules/admin/mod_ks_usergroups/js/users.js
+++ packages/modules/admin/mod_ks_usergroups/js/users.js
@@ -3,7 +3,7 @@ var UserGroupsModule='';
 jQuery(document).ready(function(){
 
 	UserGroupsModule=new KMListModule({
-		'module':'mod_km_usergroups',
+		'module':'mod_ks_usergroups',
 		'view':'users',
 		'table':'users',
 		'sortable':false
