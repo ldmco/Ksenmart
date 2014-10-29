c6a6061a057dfcc0df79d950a2c30db308f20f7d
Yoji <Stake1988@mail.ru>
2014-10-29 13:01:10 +0300

    KSM[Add]: Add constant comment notice

--- packages/com_ksenmart/site/views/comments/tmpl/default_mail.php
+++ packages/com_ksenmart/site/views/comments/tmpl/default_mail.php
@@ -1,21 +1,21 @@
 <table class="cellpadding">
 	<tr>
-		<td colspan="2">Информация</td>
+		<td colspan="2"><?php echo JText::_('KSM_PRODUCT_POST_A_REVIEW_INFO_LBL');?></td>
 	</tr>
 	<tr>
-		<td>Имя:</td>
+		<td><?php echo JText::_('KSM_PRODUCT_POST_A_REVIEW_NAME');?>:</td>
 		<td><?php echo $this->comment->name; ?></td>
 	</tr>	
 	<tr>
-		<td>Комментарий:</td>
+		<td><?php echo JText::_('KSM_COMMENTS_LABEL_TEXT');?>:</td>
 		<td><?php echo $this->comment->comment; ?></td>
 	</tr>
 	<tr>
-		<td>Достоинства:</td>
+		<td><?php echo JText::_('KSM_PRODUCT_POST_A_REVIEW_HIGHS_LBL');?>:</td>
 		<td><?php echo $this->comment->good; ?></td>
 	</tr>
 	<tr>
-		<td>Недостатки:</td>
+		<td><?php echo JText::_('KSM_PRODUCT_POST_A_REVIEW_LOWS_LBL');?>:</td>
 		<td><?php echo $this->comment->bad; ?></td>
 	</tr>
 </table>	
