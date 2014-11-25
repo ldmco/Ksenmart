e85e2a9887a192ea85ac5b0bb17442ac61456f55
Yoji <Stake1988@mail.ru>
2014-11-25 16:27:55 +0300

    KSM[FIX]: Fix total count products

--- packages/com_ksenmart/site/views/search/tmpl/default.php
+++ packages/com_ksenmart/site/views/search/tmpl/default.php
@@ -3,7 +3,7 @@
     <h2 class="title">Результаты поиска:</h2>
     <?php if($this->products || $this->cat_search || $this->manufacture_search){ ?>
     <div class="search_info lead">
-        Найденно <?php echo !empty($this->products)?count($this->products).' товаров':''; ?><?php echo !empty($this->cat_search)?', '.count($this->cat_search).' категорий':''; ?><?php echo !empty($this->manufacture_search)?' и '.count($this->manufacture_search).' производитель':''; ?>
+        Найденно <?php echo !empty($this->products)?$this->pagination->total.' товаров':''; ?><?php echo !empty($this->cat_search)?', '.count($this->cat_search).' категорий':''; ?><?php echo !empty($this->manufacture_search)?' и '.count($this->manufacture_search).' производитель':''; ?>
     </div>
     <?php } ?>
     <?php echo $this->loadTemplate('cat_search'); ?>
