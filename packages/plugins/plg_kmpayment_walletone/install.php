df89d5a317fadbd90a5161dd653c2e13aa8e0d89
Кирилл <kirill.bereza@yandex.ru>
2014-12-16 13:32:52 +0600

    KSC[FIX]: Install WalletOne on update pkg

--- packages/plugins/plg_kmpayment_walletone/install.php
+++ packages/plugins/plg_kmpayment_walletone/install.php
@@ -10,8 +10,29 @@ class plgKMPaymentWalletoneInstallerScript {
         
         if (!JFolder::move($path . 'helpers', JPATH_ROOT . DS . 'administrator' . DS . 'components' . DS . 'com_ksenmart' . DS . 'helpers')) {
             $app->enqueueMessage('Couldnt move file');
-        }       
+        }
         
         JFolder::delete($path);
+    }
+
+    public function update($parent) {
+    	if(version_compare($parent->get('manifest')->version, '1.0', '==')){
+	        if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+
+	        $app = JFactory::getApplication();
+	        $path = JPATH_ROOT . DS . 'plugins' . DS . 'kmpayment' . DS . 'walletone' . DS . 'install' . DS;
+
+	        if (!JFolder::move($path . 'helpers', JPATH_ROOT . DS . 'administrator' . DS . 'components' . DS . 'com_ksenmart' . DS . 'helpers')) {
+	            $app->enqueueMessage('Couldnt move file');
+	        }
+	        JFolder::delete($path);
+
+        	$db = $parent->get('db');
+	        $table = JTable::getInstance('extension');
+	        $table->load(array('name' => 'walletone', 'folder' => 'kmpayment'));
+	        $table->save(array(
+                'enabled' => 1
+            ));
+    	}
     }
 }
