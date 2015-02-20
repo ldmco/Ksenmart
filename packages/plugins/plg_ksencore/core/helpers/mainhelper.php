779cbbef41b8c2d91221a0d2b4b1bd134ab4b4ca
Кирилл <kirill.bereza@yandex.ru>
2015-02-20 15:57:36 +0600

    KSC[FIX]: MainHelper refactoring

--- packages/plugins/plg_ksencore/core/helpers/mainhelper.php
+++ packages/plugins/plg_ksencore/core/helpers/mainhelper.php
@@ -1,22 +1,14 @@
 <?php defined('_JEXEC') or die;
 
-class KSMainhelper {
-
-    private $ext_name_com   = null;
-    private $ext_prefix     = null;
-	private $helper_name    = null;
-	
-	
-    public static function onExecuteBefore($function = null, $ext_prefix, $helper_name, $vars = array()) {
-
-        JDispatcher::getInstance()->trigger('onBeforeExecuteHelper' . strtoupper($ext_prefix) . $helper_name . $function, $vars);
-        
+class KSCoreHelper {
+    
+    public static function onExecuteBefore($vars = array()) {
+        $trace = debug_backtrace(false);
+        JDispatcher::getInstance()->trigger('onBeforeExecuteHelper' . get_called_class() . ucfirst($trace[1]['function']), $vars);
     }
-
-    public static function onExecuteAfter($function = null, $ext_prefix, $helper_name, $vars = array()) {
-
-        JDispatcher::getInstance()->trigger('onAfterExecuteHelper' . strtoupper($ext_prefix) . $helper_name . $function, $vars);
-        
+    
+    public static function onExecuteAfter($vars = array()) {
+        $trace = debug_backtrace(false);
+        JDispatcher::getInstance()->trigger('onAfterExecuteHelper' . get_called_class() . ucfirst($trace[1]['function']), $vars);
     }
-   
 }
