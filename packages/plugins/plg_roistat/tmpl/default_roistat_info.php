11772ed55d88ed375d51b97be90f66b9730309d8
Кирилл <kirill.bereza@yandex.ru>
2015-03-03 14:57:58 +0600

    KSC[FIX]: Url on ROIStat cp

--- packages/plugins/plg_roistat/tmpl/default_roistat_info.php
+++ packages/plugins/plg_roistat/tmpl/default_roistat_info.php
@@ -1,4 +1,4 @@
 <?php defined('_JEXEC') or die; ?>
 <div class="info">
-	<a href="http://cloud.roistat.com/project/<?php echo $this->project; ?>" title="Перейти в панель управления Roistat" target="_blank">ROIstat <i class="icon-out-2"></i></a>&nbsp; успешно подключен
+	<a href="//cloud.roistat.com/projects/<?php echo $this->project; ?>" title="Перейти в панель управления Roistat" target="_blank">ROIstat <i class="icon-out-2"></i></a>&nbsp; успешно подключен
 </div>
