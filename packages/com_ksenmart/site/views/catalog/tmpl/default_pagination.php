f756bbb8e8fcd13dfd9deedbfbf6fc34eb7880f9
Кирилл <kirill.bereza@yandex.ru>
2015-04-21 17:05:49 +0600

    KSM[FIX]: Refact pagination template

--- packages/com_ksenmart/site/views/catalog/tmpl/default_pagination.php
+++ packages/com_ksenmart/site/views/catalog/tmpl/default_pagination.php
@@ -1,9 +1,7 @@
-<?
-defined( '_JEXEC' ) or die( '=;)' );
-if (!empty($this->pagination)){
-    //print_r($this->pagination);
-?>
-<div class="pagination pagination-centered">
-	<?php echo $this->pagination->getPagesLinks(); ?>
-</div>	
-<? } ?>
+<?php defined('_JEXEC') or die; ?>
+
+<?php if (!empty($this->pagination)): ?>
+	<div class="pagination pagination-centered">
+		<?php echo $this->pagination->getPagesLinks(); ?>
+	</div>
+<?php endif; ?>
