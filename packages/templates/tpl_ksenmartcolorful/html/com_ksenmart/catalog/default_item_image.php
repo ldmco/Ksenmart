8c0e314c51ad3fc7c906ec81e811f1f05965b857
TakT <kirill.bereza@yandex.ru>
2015-06-23 21:22:42 +0600

    KSC[FIX]: Labels from list catalog view

--- packages/templates/tpl_ksenmartcolorful/html/com_ksenmart/catalog/default_item_image.php
+++ packages/templates/tpl_ksenmartcolorful/html/com_ksenmart/catalog/default_item_image.php
@@ -9,7 +9,9 @@ defined('_JEXEC') or die;
 <a href="<?php echo $this->product->link; ?>" title="<?php echo $this->product->title; ?>">
 	<img src="<?php echo $this->product->small_img; ?>" alt="" class="span12" />
 </a>
-<?php echo ($this->product->hot == 1?'<span class="hit"></span>':'')?>
-<?php echo ($this->product->recommendation == 1?'<span class="super"></span>':''); ?>
-<?php echo ($this->product->new == 1?'<span class="new"></span>':''); ?>
-<?php echo ($this->product->promotion == 1?'<span class="act"></span>':''); ?>
+<div class="productFlags">
+	<?php echo ($this->product->hot == 1 ? '<span class="hit"></span>' : ''); ?>
+	<?php echo ($this->product->recommendation == 1 ? '<span class="super"></span>' : ''); ?>
+	<?php echo ($this->product->new == 1 ? '<span class="new"></span>' : ''); ?>
+	<?php echo ($this->product->promotion == 1 ? '<span class="act"></span>' : ''); ?>
+</div>
