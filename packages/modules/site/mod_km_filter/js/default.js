aee96723d98e0e132984e214e7fa3e0010ac5f1c
pasha <tominpasha@mail.ru>
2016-05-24 18:18:59 +0600

    KSM[FIX];renew filter module

--- packages/modules/site/mod_km_filter/js/default.js
+++ packages/modules/site/mod_km_filter/js/default.js
@@ -5,15 +5,15 @@ jQuery(document).ready(function() {
 
 	if (typeof(view) !== "undefined"){
 
-		jQuery('.mod_ksm_filter .tracker').slider({
+		jQuery('.ksm-module-filter-block-prices-tracker').slider({
 			range: true,
 			min: price_min,
 			max: price_max,
 			step: 100,
 			values: [price_less, price_more],
 			slide: function(event, ui) {
-				jQuery('.mod_ksm_filter input[name="price_less"]').val(ui.values[0]);
-				jQuery('.mod_ksm_filter input[name="price_more"]').val(ui.values[1]);
+				jQuery('.ksm-module-filter-block-slider input[name="price_less"]').val(ui.values[0]);
+				jQuery('.ksm-module-filter-block-slider input[name="price_more"]').val(ui.values[1]);
 			},
 			stop: function(event, ui) {
 				priceTimer = false;
@@ -21,7 +21,7 @@ jQuery(document).ready(function() {
 			}
 		});
 
-		jQuery('.mod_ksm_filter .prices input[type="text"]').keydown(function(e) {
+		jQuery('.ksm-module-filter-block-prices input[type="text"]').keydown(function(e) {
 			keynum = e.which;
 			keynum = parseInt(keynum);
 			if (keynum == 13)
@@ -56,7 +56,7 @@ jQuery(document).ready(function() {
 jQuery(window).load(function() {
 
 	if (typeof(view) !== "undefined"){
-		var form = jQuery('.mod_ksm_filter form');
+		var form = jQuery('.ksm-module-filter form');
 		var filter_url = URI_ROOT + 'index.php?option=com_ksenmart&view=catalog';
 		var formdata = form.serialize();
 		filter_url += '&' + formdata;
@@ -71,69 +71,65 @@ jQuery(window).load(function() {
 });
 
 function KMChangeFilter(obj) {
-    var form = jQuery('.mod_ksm_filter form');
-    var item = jQuery(obj).parents('.item');
-	var filter_button = jQuery(obj).is('.filter-button');
+    var form = jQuery('.ksm-module-filter form');
+    var item = jQuery(obj).is('.ksm-module-filter-block-listing-item') ? jQuery(obj) : jQuery(obj).parents('.ksm-module-filter-block-listing-item');
+	var filter_button = jQuery(obj).is('.ksm-module-filter-button-filter');
     var filter_url = URI_ROOT + 'index.php?option=com_ksenmart&view=catalog';
     var page_url = 'index.php?option=com_ksenmart&view=catalog';
 	
 	if (item.find('input').is('[type="radio"]')) {
 		if (item.is('.active')) {
 			item.removeClass('active');
-			item.parents('li').removeClass('active');
 			item.find('input').removeAttr('checked');
 		} else {
-			item.parents('.filter_box').find('.active').removeClass('active');
+			item.parents('.ksm-module-filter-block').find('.active').removeClass('active');
 			item.addClass('active');
-			item.parents('li').addClass('active');
 		}	
 	} else {
 		if (item.is('.active')) {
 			item.removeClass('active');
-			item.parents('li').removeClass('active');
 		} else {
 			item.addClass('active');
-			item.parents('li').addClass('active');
 		}
 	}	
 
-    var price_less = parseInt(jQuery('.mod_ksm_filter input[name="price_less"]').val());
-    var price_more = parseInt(jQuery('.mod_ksm_filter input[name="price_more"]').val());
+    var price_less = parseInt(jQuery('.ksm-module-filter input[name="price_less"]').val());
+    var price_more = parseInt(jQuery('.ksm-module-filter input[name="price_more"]').val());
     if (isNaN(price_less))
         price_less = 0;
     if (isNaN(price_more))
         price_more = 0;
     if (price_less < price_min) {
         price_less = price_min;
-        jQuery('.mod_ksm_filter input[name="price_less"]').val(price_less);
+        jQuery('.ksm-module-filter input[name="price_less"]').val(price_less);
     }
     if (price_less > price_max) {
         price_less = price_max;
-        jQuery('.mod_ksm_filter input[name="price_less"]').val(price_less);
+        jQuery('.ksm-module-filter input[name="price_less"]').val(price_less);
     }
     if (price_more < price_min) {
         price_more = price_min;
-        jQuery('.mod_ksm_filter input[name="price_more"]').val(price_more);
+        jQuery('.ksm-module-filter input[name="price_more"]').val(price_more);
     }
     if (price_more > price_max) {
         price_more = price_max;
-        jQuery('.mod_ksm_filter input[name="price_more"]').val(price_more);
+        jQuery('.ksm-module-filter input[name="price_more"]').val(price_more);
     }
     if (price_less > price_more) {
         var tmp = price_less;
         price_less = price_more;
         price_more = tmp;
-        jQuery('.mod_ksm_filter input[name="price_less"]').val(price_less);
-        jQuery('.mod_ksm_filter input[name="price_more"]').val(price_more);
+        jQuery('.ksm-module-filter input[name="price_less"]').val(price_less);
+        jQuery('.ksm-module-filter input[name="price_more"]').val(price_more);
     }
     if (price_min != price_max) {
-        var slider_price_less = jQuery('.mod_ksm_filter .tracker').slider('values', 0);
-        var slider_price_more = jQuery('.mod_ksm_filter .tracker').slider('values', 1);
+        var slider_price_less = jQuery('.ksm-module-filter-block-prices-tracker').slider('values', 0);
+        var slider_price_more = jQuery('.ksm-module-filter-block-prices-tracker').slider('values', 1);
         if (price_less != slider_price_less)
-            jQuery('.mod_ksm_filter .tracker').slider('values', 0, price_less);
+            jQuery('.ksm-module-filter-block-prices-tracker').slider('values', 0, price_less);
 
         if (price_more != slider_price_more)
-            jQuery('.mod_ksm_filter .tracker').slider('values', 1, price_more);
+            jQuery('.ksm-module-filter-block-prices-tracker').slider('values', 1, price_more);
     }
 
     var formdata = form.serialize();
@@ -168,58 +164,77 @@ function KMChangeFilter(obj) {
 }
 
 function KMSetFilter(filter_url, replace_content) {
-    var form = jQuery('.mod_ksm_filter form');
+    var form = jQuery('.ksm-module-filter form');
 
     jQuery.ajax({
         url: filter_url + '&task=catalog.filter_products',
         dataType: 'json',
         success: function(data) {
             if (replace_content)
-                jQuery('.catalog').replaceWith(data.html);
+                jQuery('.ksm-catalog').replaceWith(data.html);
 
             var properties = data.properties;
             var manufacturers = data.manufacturers;
             var countries = data.countries;
             var props = [];
 
-            form.find('.manufacturer').hide();
-            for (var k = 0; k < manufacturers.length; k++)
-                form.find('.manufacturer_' + manufacturers[k]).show();
 
-            form.find('.country').hide();
-            for (var k = 0; k < countries.length; k++)
-                form.find('.country_' + countries[k]).show();
+			form.find('.ksm-module-filter-block-manufacturers .ksm-module-filter-block-listing-item').hide();
+			for (var k = 0; k < manufacturers.length; k++)
+			{
+				form.find('.ksm-module-filter-block-manufacturers .ksm-module-filter-block-listing-item[data-id="' + manufacturers[k] + '"]').show();				
+			}				
+			if (manufacturers.length > 0)
+			{
+				form.find('.ksm-module-filter-block-manufacturers').show();
+			}
+			else
+			{
+				form.find('.ksm-module-filter-block-manufacturers').hide();
+			}
 
-            form.find('.property').each(function() {
-                jQuery(this).find('.property_value').addClass('inactive');
-                jQuery(this).find('.property_value').hide();
+			form.find('.ksm-module-filter-block-countries .ksm-module-filter-block-listing-item').hide();
+			for (var k = 0; k < countries.length; k++)
+			{
+				form.find('.ksm-module-filter-block-countries .ksm-module-filter-block-listing-item[data-id="' + countries[k] + '"]').show();				
+			}				
+			if (countries.length > 0)
+			{
+				form.find('.ksm-module-filter-block-countries').show();
+			}
+			else
+			{
+				form.find('.ksm-module-filter-block-countries').hide();
+			}
+
+            form.find('.ksm-module-filter-block-property').each(function(){
+				jQuery(this).find('.ksm-module-filter-block-listing-item').hide();
+				jQuery(this).hide();
             });
-            for (var key in properties) {
+            for (var key in properties) 
+			{
                 props = properties[key];
-                for (var k = 0; k < props.length; k++) {
-                    form.find('.property_value_' + props[k]).removeClass('inactive');
-                    form.find('.property_value_' + props[k]).show();
+                for(var k = 0; k < props.length; k++) 
+				{
+					form.find('.ksm-module-filter-block-property-'+key+' .ksm-module-filter-block-listing-item[data-id="' + props[k] + '"]').show();
                 }
+				if (props.length > 0)
+				{
+					form.find('.ksm-module-filter-block-property-'+key).show();
+				}
             }
-            form.find('.property').each(function() {
-                if (jQuery(this).find('.property_value').length != jQuery(this).find('.inactive').length)
-                    jQuery(this).show();
-                else
-                    jQuery(this).hide();
-            });
-			form.show();
         }
     });
 }
 
 function KMClearFilter(){
-	var form = jQuery('.mod_ksm_filter form');
+	var form = jQuery('.ksm-module-filter form');
 	
-	jQuery('.mod_ksm_filter input[name="price_less"]').val(price_min);
-	jQuery('.mod_ksm_filter input[name="price_more"]').val(price_max);	
-	form.find('.manufacturers, .properties, .countries').find('.active').removeClass('active');
-	form.find('.manufacturers, .properties, .countries').find('input[type="checkbox"], input[type="radio"]').removeAttr('checked');
-	form.find('.manufacturers, .properties, .countries').find('select').val('');
+	form.find('input[name="price_less"]').val(price_min);
+	form.find('input[name="price_more"]').val(price_max);	
+	form.find('.ksm-module-filter-block-manufacturers, .ksm-module-filter-block-properties, .ksm-module-filter-block-countries').find('.active').removeClass('active');
+	form.find('.ksm-module-filter-block-manufacturers, .ksm-module-filter-block-properties, .ksm-module-filter-block-countries').find('input[type="checkbox"], input[type="radio"]').removeAttr('checked');
+	form.find('.ksm-module-filter-block-manufacturers, .ksm-module-filter-block-properties, .ksm-module-filter-block-countries').find('select').val('');
 	
 	KMChangeFilter('clear');
 }
--- packages/modules/site/mod_km_filter/mod_km_filter.php
+++ packages/modules/site/mod_km_filter/mod_km_filter.php
@@ -13,31 +13,32 @@ if (!class_exists('KsenmartHtmlHelper')) {
 }
 KsenmartHtmlHelper::AddHeadTags();
 
-require_once dirname(__file__) . '/helper.php';
-$modKsenmartSearchHelper = new modKsenmartSearchHelper();
-
-$modKsenmartSearchHelper->init($params);
-$mod_params     = $modKsenmartSearchHelper->mod_params;
-$price_min      = $modKsenmartSearchHelper->price_min;
-$price_max      = $modKsenmartSearchHelper->price_max;
-$manufacturers  = $modKsenmartSearchHelper->manufacturers;
-$countries      = $modKsenmartSearchHelper->countries;
-$properties     = $modKsenmartSearchHelper->properties;
-$class_sfx      = htmlspecialchars($params->get('moduleclass_sfx', ''));
-$form_action    = JRoute::_('index.php?option=com_ksenmart&view=catalog&Itemid=' . KSSystem::getShopItemid());
-
-$price_less = JRequest::getVar('price_less', $price_min);
-$price_more = JRequest::getVar('price_more', $price_max);
-$categories = $modKsenmartSearchHelper->categories;
-JArrayHelper::toInteger($categories);
-$order_type = JRequest::getVar('order_type', 'ordering');
-$order_dir  = JRequest::getVar('order_dir', 'asc');
-
+$app = JFactory::getApplication();
 $km_params = JComponentHelper::getParams('com_ksenmart');
 $document  = JFactory::getDocument();
 $document->addScript(JURI::root() . 'modules/mod_km_filter/js/default.js');
 if($km_params->get('modules_styles', true)){
 	$document->addStyleSheet(JURI::base().'modules/mod_km_filter/css/default.css');
 }
+
+require_once dirname(__file__) . '/helper.php';
+$modKMFilterHelper = new modKMFilterHelper();
+
+$modKMFilterHelper->init($params);
+$mod_params     = $modKMFilterHelper->mod_params;
+$price_min      = $modKMFilterHelper->price_min;
+$price_max      = $modKMFilterHelper->price_max;
+$manufacturers  = $modKMFilterHelper->manufacturers;
+$countries      = $modKMFilterHelper->countries;
+$properties     = $modKMFilterHelper->properties;
+$categories     = $modKMFilterHelper->categories;
+$class_sfx      = htmlspecialchars($params->get('moduleclass_sfx', ''));
+$form_action    = JRoute::_('index.php?option=com_ksenmart&view=catalog&Itemid=' . KSSystem::getShopItemid());
+
+JArrayHelper::toInteger($categories);
+$price_less = $app->input->get('price_less', $price_min);
+$price_more = $app->input->get('price_more', $price_max);
+$order_type = $app->input->get('order_type', 'ordering');
+$order_dir  = $app->input->get('order_dir', 'asc');
 
 require JModuleHelper::getLayoutPath('mod_km_filter', $params->get('layout', 'default'));
